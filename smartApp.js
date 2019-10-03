d157c9d00934bee6b2281751c320627c2fa1ce9c
busrasariguzel <busra.sariguzel@codeimmersives.com>
2019-10-03 14:17:59 -0400

    first draft

--- smartApp.js
+++ smartApp.js
@@ -1,4 +1,36 @@
-function  {
+const city = process.argv[2];
+// const city = 'London';
+// console.log(city)
 
 
+function weather(city) {
+    if (city==='Istanbul') {
+        return '36C + 67F';
+    } else if (city==='London') {
+        return "13C 55F";
+    } else if (city==='NYC') {
+        return "12C + 55F";
+    } else if (city==='Tokyo'){
+        return "24C 75F";
+    } else if (city==='HongKong'){
+        return "29C 84F";
+    } else if (city=='Paris'){
+        return "12C 54F";
+    } else if (city==='Moscow'){
+        return "16C 60F";
+    } else if (city==='Shanghai'){
+        return "23C 74F"
+    } else if (city==='Barcelona'){
+        return "22C 72F"
+    } else 
+        return "Sorry! I don't know about that city."
+    
 }
+
+// weather(userInput);
+
+
+console.log(weather(London)); 
+
+
+
